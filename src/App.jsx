35134f70796cd7b7dd1ff52a3f9c433b58ffaad4
John Doe <johndoe@example.com>
2025-08-13 08:34:37 +0200

    added recipes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
